f0c117bba3dd49986eb10c82357d7e2377e3cc0f
Alexis Lazcano <0370102@cegepmontpetit.ca>
2024-01-31 17:10:30 -0500

    COMPLETÉ Labo2 / FCT de recherche d'artistes similaires

--- labo2/src/app/app.component.ts
+++ labo2/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent {
   // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
   result : boolean = false;
   artist : string = "";
+  similarArtists : string[] = [];
 
   // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
   // Le constructeur devra être ajouté ici
@@ -20,13 +22,21 @@ export class AppComponent {
   constructor(public http : HttpClient){}
 
   searchArtist():void{
+    this.request();
     this.result = true;
-	
+  }
     // ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 	  // La requête HTTP devra être ajoutée ici
     // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-
-  }
+    async request() : Promise<void>{
+      
+      let x = await lastValueFrom(this.http.get<any>("https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+this.artist+"&api_key=e34ebf8561ba7c653a21d1d99a1a0070&format=json"));
+      console.log(x);
+      for(let a of x.similarartists.artist){
+        this.similarArtists.push(a.name);
+      }
+    
+    }
 
   newSearch():void{
     this.result = false;
